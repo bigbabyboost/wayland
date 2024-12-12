c6525a79e0fbe16e32e2f98a0b4d52eba6600607
bigbabyboost <ah0199004@gmail.com>
2024-12-13 04:21:26 +0700

    bump recipe

--- files/scripts/installkernelcachyos.sh
+++ files/scripts/installkernelcachyos.sh
@@ -1,7 +1,7 @@
 set -oue pipefail
 
 curl -Lo bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo
-sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum/repos.d/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo
+sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo
 
 rpm-ostree override remove \
     kernel \
